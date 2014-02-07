264304d7d3f9eae84098dbd25905124c2645bd8d
iCodr8 <icodr8@gmail.com>
2014-02-07 17:20:33 +0100

    added delete callback

--- library/Crosstabs/LeftTable.php
+++ library/Crosstabs/LeftTable.php
@@ -45,6 +45,53 @@ class LeftTable extends \Controller
      */
     public static function deleteCallback(\DataContainer $dc)
     {
-        // Todo
+        if (!$dc->activeRecord) {
+            return;
+        }
+
+        $intId = $dc->activeRecord->id;
+        $strTable = $dc->table;
+
+        if (isset($GLOBALS['TL_DCA'][$dc->table]['fields'])) {
+            foreach ($GLOBALS['TL_DCA'][$dc->table]['fields'] as $k => $v) {
+                if (!isset($v['crossTable']) || !isset($v['crossCurrentKey'])) {
+                    continue;
+                }
+
+                // Get model name
+                $strModel = \Model::getClassFromTable($v['crossTable']);
+
+                // Check if model exists
+                if (!class_exists($strModel)) {
+                    return;
+                }
+
+                // Get model object
+                $objModel = $strModel::findBy($v['crossCurrentKey'], $intId);
+
+                if ($objModel !== null) {
+                    // Get tl_undo data
+                    $objUndo = \Database::getInstance()->prepare("SELECT * FROM tl_undo WHERE fromTable=? ORDER BY id DESC")->limit(1)->execute($dc->table);
+                    $arrSet = $objUndo->row();
+
+                    // Deserialize tl_undo data
+                    $arrSet['data'] = deserialize($arrSet['data']);
+
+                    while ($objModel->next()) {
+                        // Add cross table record to undo data
+                        $arrSet['data'][$v['crossTable']][] = $objModel->row();
+
+                        // Delete cross table item
+                        $objModel->delete();
+                    }
+
+                    // Serialize tl_undo data
+                    $arrSet['data'] = serialize($arrSet['data']);
+
+                    // Update tl_undo
+                    \Database::getInstance()->prepare("UPDATE tl_undo %s WHERE id=?")->set($arrSet)->execute($objUndo->id);
+                }
+            }
+        }
     }
 }
