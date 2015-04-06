3debe52e2d1d1db5c7ec14da859482fd9bff1682
iCodr8 <icodr8@gmail.com>
2015-04-06 22:37:59 +0200

    added mcw support and optimized tlw

---
+++ system/modules/crosstabs/library/Crosstabs/Data.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Extension for Contao Open Source CMS
+ *
+ * Copyright (c) 2014 Daniel Kiesel
+ *
+ * @package Crosstabs
+ * @link    https://github.com/craffft/contao-crosstabs
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+/**
+ * Namespace
+ */
+namespace Crosstabs;
+
+/**
+ * Class Data
+ *
+ * @copyright  Daniel Kiesel 2014
+ * @author     Daniel Kiesel <daniel@craffft.de>
+ * @deprecated This class is deprecated and will be removed on the next release! Use \Crosstabs\TableLookupWizard\DataHandler
+ */
+class Data extends \Crosstabs\DataHandler\TableLookupWizard
+{
+
+}
